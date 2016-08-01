7a23a9fe6dfde2b3e76158363740b02469893538
Phil Chapman <phil.chapman@cruk.manchester.ac.uk>
2016-08-01 16:22:32 +0100

    Version 0.1.3: Added gather.PharmacoGx function

---
+++ R/gather.PharmacoGx.R
@@ -0,0 +1,49 @@
+#' gather.PharmacoGx
+#'
+#' @param x PharmacoGx object
+#' @param sample_ids A vector of sample ids.  Default is NULL (don't filter on sample id)
+#' @param gene_ids A vector of gene ids.  Default is NULL (don't filter on gene id)
+#' @param resp_ids A vector of response ids.  Default is NULL (don't filter on response id)
+#' @param sample_col Name of the column in the pData data frame to use for filtering on sample id
+#' @param gene_col Name of the column in the rowData data frame to use for filtering on gene id
+#' @param resp_col Response variable to retrieve
+#' @param data_types Named of the componentd of the PharmacoGx object to gather
+#' @param resp_df Data frame to process response data from (rather than PharmcaGx object)
+#'
+#' @return a tibble
+#' @export
+#'
+#' @examples
+#' data('CCLEsmall', package='PharmacoGx')
+#'
+#' gather.PharmacoGx(CCLEsmall, sample_ids=c('143B', '23132-87'), gene_ids=c('BRAF', 'EGFR'),
+#'  data_types=c('rna', 'mutation'), gene_col=c('Symbol', 'Symbol'))
+#'
+#' gather.PharmacoGx(CCLEsmall, sample_ids=c('CHL-1', 'SW1573'), gene_ids=c('BRAF', 'EGFR'),
+#'  resp_ids=c("AEW541","Nilotinib","PHA-665752","lapatinib"),
+#'  data_types=c('rna', 'mutation'), gene_col=c('Symbol', 'Symbol'), resp_col='ic50_published')
+#'
+#'
+#'
+gather.PharmacoGx <- function(x, sample_ids=NULL, gene_ids=NULL, resp_ids=NULL,
+                              sample_col='cellid', gene_col=c('Symbol', 'gene_name'),
+                              resp_col='ic50_published', data_types=c('rna', 'mutation'), resp_df=NULL) {
+
+
+    stopifnot(length(gene_col) == length(data_types))
+
+    genetic_data <- purrr::map2(data_types, gene_col, function(z1, z2) {
+        gather_assay.PharmacoGx(x, sample_ids=sample_ids, gene_ids=gene_ids,
+                                data_type=z1, sample_col=sample_col, gene_col=z2)
+        }) %>%
+        dplyr::bind_rows()
+
+    if(is.null(resp_ids)) {
+        return(genetic_data)
+    }  else {
+        drug_data <- gather_response.PharmacoGx(x, sample_ids=sample_ids, resp_ids=resp_ids, resp_col=resp_col)
+        dplyr::bind_rows(genetic_data, drug_data)
+    }
+
+
+}
