1fb1141c8b2846bc6feb77f22682e2559ec02b34
Phil Chapman <phil.chapman@cruk.manchester.ac.uk>
2016-08-16 18:06:59 +0100

    Verison 0.2.6: working version of makeRespVsGeneticTibble.data.frame

--- R/makeRespVsGeneticTibble.data.frame.R
+++ R/makeRespVsGeneticTibble.data.frame.R
@@ -20,10 +20,10 @@
 #' resp_data <- gather_response.PharmacoSet(CCLEsmall, sample_ids=PharmacoGx::cellNames(CCLEsmall),
 #'                          resp_ids=c('AEW541', 'Nilotinib'), resp_col='ic50_published')
 #'
-#' rvg_df <- makeRespVsGeneticTibble.data.frame(df=dat1, df2=dat2, sample_ids=PharmacoGx::cellNames(CCLEsmall), gene_ids='RBM5',
+#' rvg_df <- makeRespVsGeneticTibble.data.frame(df=genetic_data, df2=resp_data, sample_ids=PharmacoGx::cellNames(CCLEsmall), gene_ids='RBM5',
 #'  data_types='rna', resp_ids='Nilotinib')
 #' rvg_df
-#' CancerCellLines::plotGeneticVsGeneticPoint(gvg_df)
+#' CancerCellLines::plotRespVsGeneticPoint(rvg_df)
 makeRespVsGeneticTibble.data.frame <- function(df, df2=NULL, sample_ids=NULL, gene_ids=NULL, data_types=NULL, compound_ids=NULL, endpoints=NULL, resp_ids=NULL) {
 
     if(is.null(df2)) {df2 <- df}
@@ -35,11 +35,11 @@ makeRespVsGeneticTibble.data.frame <- function(df, df2=NULL, sample_ids=NULL, ge
     }
 
     if(!is.null(gene_ids)) {
-        df <- df %>% dplyr::filter(assayed_id %in% gene1)
+        df <- df %>% dplyr::filter(assayed_id %in% gene_ids)
     }
 
     if(!is.null(data_types)) {
-        df <- df %>% dplyr::filter(data_type %in% data_type1)
+        df <- df %>% dplyr::filter(data_type %in% data_types)
     }
 
     if(!is.null(compound_ids) & 'compound_id' %in% colnames(df2)) {
@@ -50,26 +50,25 @@ makeRespVsGeneticTibble.data.frame <- function(df, df2=NULL, sample_ids=NULL, ge
         df2 <- df2 %>% dplyr::filter(endpoint %in% endpoints)
     }
 
-    if(!is.null(resp_ids) & 'resp_id' %in% colnames(df2)) {
-        df2 <- df2 %>% dplyr::filter(resp_id %in% resp_ids)
+    if(!is.null(resp_ids) & 'assayed_id' %in% colnames(df2)) {
+        df2 <- df2 %>% dplyr::filter(assayed_id %in% resp_ids)
     }
 
-    #modifying   ###  THIS IS THE BIT TO DO ON THE TRAIN!!!
+    #modify the data frame
     df <- df %>%
-        dplyr::transmute(sample_id, gene1 = assayed_id,
-                         feature_type1 = data_type,
-                         feature_name1 = paste(assayed_id, data_type, sep = "_"),
-                         feature_value1 = value,
-                         feature_original1 = original)
+        dplyr::transmute(sample_id,assayed_id,
+                         feature_type = data_type,
+                         feature_name = paste(assayed_id, data_type, sep = "_"),
+                         feature_value = value,
+                         feature_original = original)
 
     df2 <- df2 %>%
-        dplyr::transmute(sample_id, gene2 = assayed_id,
-                         feature_type2 = data_type,
-                         feature_name2 = paste(assayed_id, data_type, sep = "_"),
-                         feature_value2 = value,
-                         feature_original2 = original)
+        dplyr::transmute(sample_id, resp_id = assayed_id,
+                         resp_value = value,
+                         resp_original = original)
 
 
+    #join the data frames by sample_id
     df %>%
         dplyr::inner_join(df2, by = "sample_id")
 
